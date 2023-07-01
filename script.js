b3d16306394add5e942ca059adb5eaae3977cbd6
RivaWa <137686938+RivaWa@users.noreply.github.com>
2023-07-01 18:10:53 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var php = function () {
+  document.querySelector(".php").className = "php1";
+};
+var css = function () {
+  document.querySelector(".css").className = "css1";
+};
+var htm = function () {
+  document.querySelector(".htm").className = "htm1";
+};
+setTimeout(php, 1000);
+setTimeout(css, 3000);
+setTimeout(htm, 5000);
